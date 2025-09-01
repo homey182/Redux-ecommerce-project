c2b9924ff2a0271b57da6e7fa7900f7ab2698aa2
home narayan thakur <homenarayant@gmail.com>
2025-09-01 10:17:09 +0545

    redux-ecommerce-project setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Mainroutes from "./routes/Mainroutes";
+import axios from "./api/axios";
+
+const App = () => {
+  const getData = async () => {
+    const res =  await axios.get("/comments");
+    console.log(res);
+  };
+  getData();
+
+  return (
+    <div className="bg-gray-600 h-screen  w-screen">
+      <Navbar />
+      <Mainroutes />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <NavLink>home</NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
